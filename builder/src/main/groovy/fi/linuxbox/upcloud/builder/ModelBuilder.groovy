1da7399e6ac163ede9d0d0937989aa47cd81c201
Mikko Värri <vmj@linuxbox.fi>
2016-05-09 03:13:33 +0300

    Add model builder.

---
+++ builder/src/main/groovy/fi/linuxbox/upcloud/builder/ModelBuilder.groovy
@@ -0,0 +1,90 @@
+package fi.linuxbox.upcloud.builder
+
+import org.slf4j.*
+
+import fi.linuxbox.upcloud.core.*
+
+/**
+ *
+ */
+class ModelBuilder {
+    private final Logger log = LoggerFactory.getLogger(ModelBuilder)
+
+    /**
+     * Build a new server model.
+     * <p>
+     *     ...
+     * </p>
+     * <code>
+     *     def model = server {
+     *         hostname = 'server1.example.com'
+     *         coreNumber = '1'
+     *         memoryAmount = '2048'
+     *     }
+     * </code>
+     *
+     * @param closure A closure that will configure the new server.
+     * @return The new server.
+     */
+    static def server(Closure closure = null) {
+        model 'server', closure
+    }
+
+    def methodMissing(final String name, final def args) {
+        switch (args.length) {
+            case 0:
+                return model(name)
+            case 1:
+                if (args[0] instanceof Closure)
+                    return model(name, args[0])
+        }
+        throw new MissingMethodException(name, this.class, args)
+    }
+
+    /**
+     * (Re)configure a model.
+     * <p>
+     *     ...
+     * </p>
+     * <code>
+     *     def model = configure server, {
+     *         hostname = 'server1.example.com'
+     *         coreNumber = '1'
+     *         memoryAmount = '2048'
+     *     }
+     * </code>
+     *
+     * @param model The existing model to (re)configure.
+     * @param closure A closure that will (re)configure the model.
+     * @return The same model, (re)configured.
+     */
+    static MODEL configure(MODEL model, Closure closure = null) {
+        if (closure) {
+            closure.delegate = model
+            closure.call()
+        }
+        model
+    }
+
+    /**
+     * Build a new named model.
+     * <p>
+     *     ...
+     * </p>
+     * <code>
+     *     def model = model 'server', {
+     *         hostname = 'server1.example.com'
+     *         coreNumber = '1'
+     *         memoryAmount = '2048'
+     *     }
+     * </code>
+     *
+     * @param type Type of the model to create (simple class name with first letter in lowercase)
+     * @param closure A closure that will configure the new model
+     * @return The new model.
+     */
+    static def model(String type, Closure closure = null) {
+        MODEL model = new MODEL([repr: [(type): [:]]])."$type"
+        configure model, closure
+    }
+}
---
+++ builder/src/test/groovy/fi/linuxbox/upcloud/builder/ModelBuilderSpec.groovy
@@ -0,0 +1,115 @@
+package fi.linuxbox.upcloud.builder
+
+import spock.lang.*
+
+import fi.linuxbox.upcloud.core.*
+
+import static fi.linuxbox.upcloud.builder.ModelBuilder.*
+
+/**
+ *
+ */
+@Stepwise
+class ModelBuilderSpec extends Specification{
+
+    def "Configuring an existing model"() {
+        given:
+            MODEL model = new MODEL()
+
+        when:
+            configure model, {
+                hostname = 'server1.example.com'
+                coreNumber = '1'
+                memoryAmount = '2048'
+            }
+
+        then:
+            model?.hostname == 'server1.example.com'
+            model.coreNumber == '1'
+            model.memoryAmount == '2048'
+    }
+
+    def "Configuration no-op"() {
+        given:
+            def someModel = new MODEL()
+
+        when:
+            def model = configure someModel
+
+        then:
+            model?.class.simpleName == 'MODEL'
+    }
+
+    def "Named model creation and configuration"() {
+        when:
+            def model = model 'ipAddress', {
+                hostname = 'server1.example.com'
+                coreNumber = '1'
+                memoryAmount = '2048'
+            }
+
+        then:
+            model?.class.simpleName == 'IpAddress'
+            model.hostname == 'server1.example.com'
+            model.coreNumber == '1'
+            model.memoryAmount == '2048'
+    }
+
+    def "Named model creation without configuration"() {
+        when:
+            def server = model 'portRangeStart'
+
+        then:
+            server?.class.simpleName == 'PortRangeStart'
+    }
+
+    def "Custom model creation and configuration"() {
+        given:
+            def builder = new ModelBuilder()
+
+        when:
+            def model = builder.dockerImage {
+                image = 'nginx:latest'
+                state = 'running'
+            }
+
+        then:
+            model?.class.simpleName == 'DockerImage'
+            model.image == 'nginx:latest'
+            model.state == 'running'
+    }
+
+    def "Custom model creation without configuration"() {
+        given:
+            def builder = new ModelBuilder()
+
+        when:
+            def model = builder.RancherAgent()
+
+        then:
+            model?.class.simpleName == 'RancherAgent'
+    }
+
+    def "Server creation and configuration"() {
+        when:
+            def server = server {
+                hostname = 'server1.example.com'
+                coreNumber = '1'
+                memoryAmount = '2048'
+            }
+
+        then:
+            server?.class.simpleName == 'Server'
+            server.hostname == 'server1.example.com'
+            server.coreNumber == '1'
+            server.memoryAmount == '2048'
+    }
+
+    def "Server creation without configuration"() {
+        when:
+            def model = server()
+
+        then:
+            model?.class.simpleName == 'Server'
+    }
+}
