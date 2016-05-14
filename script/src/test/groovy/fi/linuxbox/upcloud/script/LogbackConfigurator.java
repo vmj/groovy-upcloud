257bdcff5b426fa80d69b8a53969e7a3d20aaa03
Mikko Värri <vmj@linuxbox.fi>
2016-05-14 18:25:51 +0300

    Speed up tests by configuring Logback in Java.

---
+++ script/src/test/groovy/fi/linuxbox/upcloud/script/LogbackConfigurator.java
@@ -0,0 +1,30 @@
+package fi.linuxbox.upcloud.script;
+
+import ch.qos.logback.classic.*;
+import ch.qos.logback.core.status.*;
+import ch.qos.logback.core.util.*;
+
+import static ch.qos.logback.classic.Level.*;
+
+/**
+ *
+ */
+public class LogbackConfigurator
+        extends BasicConfigurator
+{
+    public LogbackConfigurator()
+    {
+        super();
+    }
+
+    @Override
+    public void configure(final LoggerContext lc)
+    {
+        super.configure(lc);
+
+        lc.getLogger(getClass().getPackage().getName()).setLevel(DEBUG);
+
+        StatusPrinter.print(lc);
+        lc.getStatusManager().add(new OnConsoleStatusListener());
+    }
+}
