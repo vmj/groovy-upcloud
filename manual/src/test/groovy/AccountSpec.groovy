f3ffa8c0ce72399fc99b6a0c9800c7babd5e7b6f
Mikko Värri <vmj@linuxbox.fi>
2017-06-17 01:08:14 +0300

    Upgrade Gradle (4.0)

--- manual/src/test/groovy/AccountSpec.groovy
+++ manual/src/test/groovy/AccountSpec.groovy
@@ -1,24 +1,47 @@
-import spock.lang.Ignore
 import spock.lang.Specification
 
 class AccountSpec extends Specification {
 
-    def ctx
+    def "scripting test"() {
+        given:
+            StringWriter content = new StringWriter()
+            GroovyShell shell = new GroovyShell(new Binding(out: new PrintWriter(content)))
 
-    def setup() {
-        // ctx = new
-    }
+        when:
+            shell.evaluate("""
+@Grab("fi.linuxbox.upcloud:groovy-upcloud-script:0.0.4")
+import fi.linuxbox.upcloud.script.*
 
-    @Ignore
-    def "test"() {
-        expect:
-            ctx.upCloud.account { resp ->
-                cond.evaluate {
-                    assert resp.META.status == 200
-                    def account = resp.account
-                    assert account.class.simpleName == 'Account'
-                    assert account.username != null
+import java.util.concurrent.*
+import static java.util.concurrent.TimeUnit.*
+def cv = new CountDownLatch(1)
+
+def ctx = new UpCloudScriptContext("foo", "bar")
+ctx.upCloud.account(
+        success: { response ->
+                println "\${response.account.username}: \${response.account.credits}"
+                cv.countDown()
+        },
+        { response, err ->
+                if (err) {
+                        println "Network error: \${err.message}"
+                        if (err.cause)
+                                err.cause.printStackTrace()
+                } else {
+                        println "HTTP status: \${response.META.status}: \${response.META.message}"
+                        if (response.error)
+                            println "Detailed error: \${response.error.errorCode}: \${response.error.errorMessage}"
                 }
-            }
+                cv.countDown()
+        })
+
+cv.await(30, SECONDS)
+ctx.close()
+""")
+
+        then:
+            content.toString() == """HTTP status: 401: Authorization Required
+Detailed error: AUTHENTICATION_FAILED: Authentication failed using the given username and password.
+"""
     }
 }
