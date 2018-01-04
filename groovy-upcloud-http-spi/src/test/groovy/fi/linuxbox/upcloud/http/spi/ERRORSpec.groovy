8a2746662131501432c879f0d818f26f15cf4d95
Mikko Värri <vmj@linuxbox.fi>
2018-01-04 16:40:06 +0200

    Move ERRORSpec to correct module

--- groovy-upcloud-http-spi/src/test/groovy/fi/linuxbox/upcloud/http/spi/ERRORSpec.groovy
+++ groovy-upcloud-http-spi/src/test/groovy/fi/linuxbox/upcloud/http/spi/ERRORSpec.groovy
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package fi.linuxbox.upcloud.core
+package fi.linuxbox.upcloud.http.spi
 
 import fi.linuxbox.upcloud.http.spi.ERROR
 import spock.lang.Specification
