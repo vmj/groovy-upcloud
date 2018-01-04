f99e19eaee4489153ad324533b2f622492492078
Mikko Värri <vmj@linuxbox.fi>
2018-01-05 00:38:10 +0200

    At API level, default request callback is optional

--- groovy-upcloud-core/src/main/groovy/fi/linuxbox/upcloud/core/AbstractSession.groovy
+++ groovy-upcloud-core/src/main/groovy/fi/linuxbox/upcloud/core/AbstractSession.groovy
@@ -24,57 +24,163 @@ import static java.util.Collections.unmodifiableMap
 
 /**
  *
- * @param <T>
+ * @param <T> Type parameter T is the return type of the
+ * {@link #request(Map&lt;?, Closure&lt;Void&gt;&gt;, java.lang.String, java.lang.String, fi.linuxbox.upcloud.core.Resource, Closure&lt;Void&gt;) request method}
  */
 @CompileStatic
 abstract class AbstractSession<T> {
-    private final Map<?, Closure<Void>> EMPTY_CBS = unmodifiableMap(emptyMap())
+    private static final Map<?, Closure<Void>> EMPTY_CBS = unmodifiableMap(emptyMap())
 
     /**
-     * Perform a HTTP GET method.
+     * Performs a HTTP GET method.
+     * <p>
+     *     This method calls the
+     *     {@link #request(Map&lt;?, Closure&lt;Void&gt;&gt;, java.lang.String, java.lang.String, fi.linuxbox.upcloud.core.Resource, Closure&lt;Void&gt;) request method}
+     *     with {@code GET} method and {@code null} resource.
+     * </p>
+     * <p>
+     *     This overload is used when the application code calls this directly.  For example:
+     * </p>
+     * <pre><code lang="groovy">
+     *     session.GET '/lb', error: { log.fatal("${it.META}") }, { res, err ->
+     *         res.loadBalancers.each { lb ->
+     *             log.info("Load balancer: ${lb.name}")
+     *         }
+     *     }
+     * </code></pre>
+     * <p>
+     *     In the example above, there is a REST API endpoint that this library does not know about: {@code /lb}.
+     *     Application can bypass the outdated library API and make REST API calls directly.
+     * </p>
      *
      * @param cbs Additional request callbacks.
      * @param path Resource path relative to the API context path, i.e. without leading slash.
      * @param cb Default request callback.
-     * @return Whatever is returned by a concrete subclass.  See {@link #request(Map,String,String,Resource,Closure)}.
+     * @return Whatever is returned by a concrete subclass from the
+     * {@link #request(Map&lt;?, Closure&lt;Void&gt;&gt;, java.lang.String, java.lang.String, fi.linuxbox.upcloud.core.Resource, Closure&lt;Void&gt;) request method}.
      */
-    T GET(Map<?, Closure<Void>> cbs, String path, Closure<Void> cb) {
+    T GET(Map<?, Closure<Void>> cbs, String path, Closure<Void> cb = null) {
         request(cbs, "GET", path, null, cb)
     }
 
     /**
-     * Perform a HTTP DELETE method.
+     * Performs a HTTP DELETE method.
+     * <p>
+     *     This method calls the
+     *     {@link #request(Map&lt;?, Closure&lt;Void&gt;&gt;, java.lang.String, java.lang.String, fi.linuxbox.upcloud.core.Resource, Closure&lt;Void&gt;) request method}
+     *     with {@code DELETE} method and {@code null} resource.
+     * </p>
+     * <p>
+     *     This overload is used when the application code calls this directly.  For example:
+     * </p>
+     * <pre><code lang="groovy">
+     *     Resource lb = // obtain a LoadBalancer...
+     *     session.DELETE "/lb/${lb.uuid}", error: { log.fatal("${it.META}") }, { res, err ->
+     *         if (err)
+     *           log.warn("Network error: ${err.message}")
+     *         else
+     *           log.info("Load balancer deleted")
+     *     }
+     * </code></pre>
+     * <p>
+     *     In the example above, there is a REST API endpoint that this library does not know about: {@code /lb}.
+     *     Application can bypass the outdated library API and make REST API calls directly.
+     * </p>
      *
      * @param cbs Additional request callbacks.
      * @param path Resource path relative to the API context path, i.e. without leading slash.
      * @param cb Default request callback.
-     * @return Whatever is returned by a concrete subclass.  See {@link #request(Map,String,String,Resource,Closure)}.
+     * @return Whatever is returned by a concrete subclass from the
+     * {@link #request(Map&lt;?, Closure&lt;Void&gt;&gt;, java.lang.String, java.lang.String, fi.linuxbox.upcloud.core.Resource, Closure&lt;Void&gt;) request method}.
      */
-    T DELETE(Map<?, Closure<Void>> cbs, String path, Closure<Void> cb) {
+    T DELETE(Map<?, Closure<Void>> cbs, String path, Closure<Void> cb = null) {
         request(cbs, "DELETE", path, null, cb)
     }
 
     /**
-     * Perform a HTTP PUT method.
+     * Performs a HTTP PUT method.
+     * <p>
+     *     This method calls the
+     *     {@link #request(Map&lt;?, Closure&lt;Void&gt;&gt;, java.lang.String, java.lang.String, fi.linuxbox.upcloud.core.Resource, Closure&lt;Void&gt;) request method}
+     *     with {@code PUT} method.
+     * </p>
+     * <p>
+     *     This overload is used when the application code calls this directly.  For example:
+     * </p>
+     * <pre><code lang="groovy">
+     *     import static fi.linuxbox.upcloud.builder.ResourceBuilder.configure
+     *
+     *     Resource lb = // obtain a LoadBalancer...
+     *
+     *     lb = configure lb {
+     *         method = 'round-robin'
+     *     }
+     *
+     *     session.PUT "/lb/${lb.uuid}", lb, error: { log.fatal("${it.META}") }, { res, err ->
+     *         if (err)
+     *           log.warn("Network error: ${err.message}")
+     *         else
+     *           log.info("Load balancer updated")
+     *     }
+     * </code></pre>
+     * <p>
+     *     In the example above, there is a REST API endpoint that this library does not know about: {@code /lb}.
+     *     Application can bypass the outdated library API and PUT the custom resource to the REST API directly.
+     * </p>
      *
      * @param cbs Additional request callbacks.
      * @param path Resource path relative to the API context path, i.e. without leading slash.
+     * @param resource Resource to send or {@code null}.
      * @param cb Default request callback.
-     * @return Whatever is returned by a concrete subclass.  See {@link #request(Map,String,String,Resource,Closure)}.
+     * @return Whatever is returned by a concrete subclass from the
+     * {@link #request(Map&lt;?, Closure&lt;Void&gt;&gt;, java.lang.String, java.lang.String, fi.linuxbox.upcloud.core.Resource, Closure&lt;Void&gt;) request method}.
      */
-    T PUT(Map<?, Closure<Void>> cbs, String path, Resource resource, Closure<Void> cb) {
+    T PUT(Map<?, Closure<Void>> cbs, String path, Resource resource, Closure<Void> cb = null) {
         request(cbs, "PUT", path, resource, cb)
     }
 
     /**
-     * Perform a HTTP POST method.
+     * Performs a HTTP POST method.
+     * <p>
+     *     This method calls the
+     *     {@link #request(Map&lt;?, Closure&lt;Void&gt;&gt;, java.lang.String, java.lang.String, fi.linuxbox.upcloud.core.Resource, Closure&lt;Void&gt;) request method}
+     *     with {@code POST} method.
+     * </p>
+     * <p>
+     *     This overload is used when the application code calls this directly.  For example:
+     * </p>
+     * <pre><code lang="groovy">
+     *     import static fi.linuxbox.upcloud.builder.ResourceBuilder.build
+     *
+     *     def lb = build 'LoadBalancer' {
+     *         name = 'web'
+     *         method = 'random'
+     *         backends = [
+     *             "192.168.1.120:80",
+     *             "192.168.1.121:80"
+     *         ]
+     *     }
+     *
+     *     session.POST '/lb', lb, error: { log.warn("${it.META}") }, { res, err ->
+     *         if (err)
+     *           log.warn("Network error: ${err.message}")
+     *         else
+     *           log.info("Load balancer created")
+     *     }
+     * </code></pre>
+     * <p>
+     *     In the example above, there is a REST API endpoint that this library does not know about: {@code /lb}.
+     *     Application can bypass the outdated library API and POST the custom resource to the REST API directly.
+     * </p>
      *
      * @param cbs Additional request callbacks.
      * @param path Resource path relative to the API context path, i.e. without leading slash.
+     * @param resource Resource to send or {@code null}.
      * @param cb Default request callback.
-     * @return Whatever is returned by a concrete subclass.  See {@link #request(Map,String,String,Resource,Closure)}.
+     * @return Whatever is returned by a concrete subclass from the
+     * {@link #request(Map&lt;?, Closure&lt;Void&gt;&gt;, java.lang.String, java.lang.String, fi.linuxbox.upcloud.core.Resource, Closure&lt;Void&gt;) request method}.
      */
-    T POST(Map<?, Closure<Void>> cbs, String path, Resource resource, Closure<Void> cb) {
+    T POST(Map<?, Closure<Void>> cbs, String path, Resource resource, Closure<Void> cb = null) {
         request(cbs, "POST", path, resource, cb)
     }
 
@@ -84,7 +190,7 @@ abstract class AbstractSession<T> {
      * @param cbs Additional request callbacks.
      * @param method HTTP method.
      * @param path Resource path relative to the API context path, i.e. without leading slash.
-     * @param resource Resource to send or null.
+     * @param resource Resource to send or {@code null}.
      * @param cb Default request callback.
      * @return Whatever is returned by a concrete subclass.
      */
@@ -95,67 +201,138 @@ abstract class AbstractSession<T> {
                                  final Closure<Void> cb)
 
     /**
-     * Calls {@link #GET(Map,String,Closure)} with an empty map.
+     * Calls {@code GET} with an empty map as additional request callbacks.
+     * <p>
+     *     This overload is typically used by the higher level API traits when the application does not pass any
+     *     additional request callbacks.
+     * </p>
+     *
+     * @param path Resource path relative to the API context path, i.e. without leading slash.
+     * @param cb Default request callback.
+     * @return Whatever is returned by a concrete subclass from the
+     * {@link #request(Map&lt;?, Closure&lt;Void&gt;&gt;, java.lang.String, java.lang.String, fi.linuxbox.upcloud.core.Resource, Closure&lt;Void&gt;) request method}.
      */
-    T GET(String path, Closure<Void> cb) {
+    T GET(String path, Closure<Void> cb = null) {
         GET(EMPTY_CBS, path, cb)
     }
 
     /**
-     * Calls {@link #GET(Map,String,Closure)} with given arguments in correct order.
+     * Calls {@code GET} with given arguments in correct order.
+     * <p>
+     *     This overload is typically used by the higher level API traits when the application does provide some
+     *     additional request callbacks.
+     * </p>
+     *
+     * @param path Resource path relative to the API context path, i.e. without leading slash.
+     * @param cbs Additional request callbacks.
+     * @param cb Default request callback.
+     * @return Whatever is returned by a concrete subclass from the
+     * {@link #request(Map&lt;?, Closure&lt;Void&gt;&gt;, java.lang.String, java.lang.String, fi.linuxbox.upcloud.core.Resource, Closure&lt;Void&gt;) request method}.
      */
-    T GET(String path, Map<?, Closure<Void>> cbs, Closure<Void> cb) {
+    T GET(String path, Map<?, Closure<Void>> cbs, Closure<Void> cb = null) {
         GET(cbs, path, cb)
     }
 
     /**
-     * Calls {@link #GET(Map,String,Closure)} with given arguments in correct order.
+     * Calls {@code DELETE} with an empty map as additional request callbacks.
+     * <p>
+     *     This overload is typically used by the higher level API traits when the application does not pass any
+     *     additional request callbacks.
+     * </p>
+     *
+     * @param path Resource path relative to the API context path, i.e. without leading slash.
+     * @param cb Default request callback.
+     * @return Whatever is returned by a concrete subclass from the
+     * {@link #request(Map&lt;?, Closure&lt;Void&gt;&gt;, java.lang.String, java.lang.String, fi.linuxbox.upcloud.core.Resource, Closure&lt;Void&gt;) request method}.
      */
-    T GET(String path, Closure<Void> cb, Map<?, Closure<Void>> cbs) {
-        GET(cbs, path, cb)
-    }
-
-    T DELETE(String path, Closure<Void> cb) {
+    T DELETE(String path, Closure<Void> cb = null) {
         DELETE(EMPTY_CBS, path, cb)
     }
 
-    T DELETE(String path, Map<?, Closure<Void>> cbs, Closure<Void> cb) {
+    /**
+     * Calls {@code DELETE} with given arguments in correct order.
+     * <p>
+     *     This overload is typically used by the higher level API traits when the application does provide some
+     *     additional request callbacks.
+     * </p>
+     *
+     * @param path Resource path relative to the API context path, i.e. without leading slash.
+     * @param cbs Additional request callbacks.
+     * @param cb Default request callback.
+     * @return Whatever is returned by a concrete subclass from the
+     * {@link #request(Map&lt;?, Closure&lt;Void&gt;&gt;, java.lang.String, java.lang.String, fi.linuxbox.upcloud.core.Resource, Closure&lt;Void&gt;) request method}.
+     */
+    T DELETE(String path, Map<?, Closure<Void>> cbs, Closure<Void> cb = null) {
         DELETE(cbs, path, cb)
     }
 
-    T DELETE(String path, Closure<Void> cb, Map<?, Closure<Void>> cbs) {
-        DELETE(cbs, path, cb)
-    }
-
-    T PUT(String path, Resource resource, Closure<Void> cb) {
+    /**
+     * Calls {@code PUT} with an empty map as additional request callbacks.
+     * <p>
+     *     This overload is typically used by the higher level API traits when the application does not pass any
+     *     additional request callbacks.
+     * </p>
+     *
+     * @param path Resource path relative to the API context path, i.e. without leading slash.
+     * @param resource Resource to send or {@code null}.
+     * @param cb Default request callback.
+     * @return Whatever is returned by a concrete subclass from the
+     * {@link #request(Map&lt;?, Closure&lt;Void&gt;&gt;, java.lang.String, java.lang.String, fi.linuxbox.upcloud.core.Resource, Closure&lt;Void&gt;) request method}.
+     */
+    T PUT(String path, Resource resource, Closure<Void> cb = null) {
         PUT(EMPTY_CBS, path, resource, cb)
     }
 
-    T PUT(String path, Map<?, Closure<Void>> cbs, Resource resource, Closure<Void> cb) {
+    /**
+     * Calls {@code PUT} with given arguments in correct order.
+     * <p>
+     *     This overload is typically used by the higher level API traits when the application does provide some
+     *     additional request callbacks.
+     * </p>
+     *
+     * @param path Resource path relative to the API context path, i.e. without leading slash.
+     * @param resource Resource to send or {@code null}.
+     * @param cbs Additional request callbacks.
+     * @param cb Default request callback.
+     * @return Whatever is returned by a concrete subclass from the
+     * {@link #request(Map&lt;?, Closure&lt;Void&gt;&gt;, java.lang.String, java.lang.String, fi.linuxbox.upcloud.core.Resource, Closure&lt;Void&gt;) request method}.
+     */
+    T PUT(String path, Resource resource, Map<?, Closure<Void>> cbs, Closure<Void> cb = null) {
         PUT(cbs, path, resource, cb)
     }
 
-    T PUT(String path, Resource resource, Map<?, Closure<Void>> cbs, Closure<Void> cb) {
-        PUT(cbs, path, resource, cb)
-    }
-
-    T PUT(String path, Resource resource, Closure<Void> cb, Map<?, Closure<Void>> cbs) {
-        PUT(cbs, path, resource, cb)
-    }
-
-    T POST(String path, Resource resource, Closure<Void> cb) {
+    /**
+     * Calls {@code POST} with an empty map as additional request callbacks.
+     * <p>
+     *     This overload is typically used by the higher level API traits when the application does not pass any
+     *     additional request callbacks.
+     * </p>
+     *
+     * @param path Resource path relative to the API context path, i.e. without leading slash.
+     * @param resource Resource to send or {@code null}.
+     * @param cb Default request callback.
+     * @return Whatever is returned by a concrete subclass from the
+     * {@link #request(Map&lt;?, Closure&lt;Void&gt;&gt;, java.lang.String, java.lang.String, fi.linuxbox.upcloud.core.Resource, Closure&lt;Void&gt;) request method}.
+     */
+    T POST(String path, Resource resource, Closure<Void> cb = null) {
         POST(EMPTY_CBS, path, resource, cb)
     }
 
-    T POST(String path, Map<?, Closure<Void>> cbs, Resource resource, Closure<Void> cb) {
-        POST(cbs, path, resource, cb)
-    }
-
-    T POST(String path, Resource resource, Map<?, Closure<Void>> cbs, Closure<Void> cb) {
-        POST(cbs, path, resource, cb)
-    }
-
-    T POST(String path, Resource resource, Closure<Void> cb, Map<?, Closure<Void>> cbs) {
+    /**
+     * Calls {@code POST} with given arguments in correct order.
+     * <p>
+     *     This overload is typically used by the higher level API traits when the application does provide some
+     *     additional request callbacks.
+     * </p>
+     *
+     * @param path Resource path relative to the API context path, i.e. without leading slash.
+     * @param resource Resource to send or {@code null}.
+     * @param cbs Additional request callbacks.
+     * @param cb Default request callback.
+     * @return Whatever is returned by a concrete subclass from the
+     * {@link #request(Map&lt;?, Closure&lt;Void&gt;&gt;, java.lang.String, java.lang.String, fi.linuxbox.upcloud.core.Resource, Closure&lt;Void&gt;) request method}.
+     */
+    T POST(String path, Resource resource, Map<?, Closure<Void>> cbs, Closure<Void> cb = null) {
         POST(cbs, path, resource, cb)
     }
 }
---
+++ groovy-upcloud-core/src/test/groovy/fi/linuxbox/upcloud/core/AbstractSessionSpec.groovy
@@ -0,0 +1,365 @@
+/*
+ * Groovy UpCloud library - Core Module
+ * Copyright (C) 2018  <mikko@varri.fi>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package fi.linuxbox.upcloud.core
+
+import spock.lang.Specification
+
+class AbstractSessionSpec extends Specification {
+    private final static Integer SUCCESS = Integer.MAX_VALUE
+    private final static Map<?, Closure<Void>> CBS = ["200": {}]
+    private final static String PATH = "/some/resource"
+    private final static Resource RESOURCE = new Resource()
+    private final static Closure<Void> CB = {}
+
+    private static class MySession extends AbstractSession<Integer> {
+        Map<?, Closure<Void>> cbs
+        String method
+        String path
+        Resource resource
+        Closure<Void> cb
+
+        @Override
+        protected Integer request(Map<?, Closure<Void>> cbs, String method, String path, Resource resource, Closure<Void> cb) {
+            this.cbs = cbs
+            this.method = method
+            this.path = path
+            this.resource = resource
+            this.cb = cb
+
+            return SUCCESS
+        }
+    }
+
+    MySession session
+
+    def setup() {
+        session = new MySession()
+    }
+
+    def "GET(Map, String, Closure)"() {
+        when:
+        def ok = session.GET(CBS, PATH, CB)
+
+        then:
+        ok == SUCCESS
+        session.cbs.is(CBS)
+        session.method == 'GET'
+        session.path.is(PATH)
+        session.resource == null
+        session.cb.is(CB)
+    }
+
+    def "GET(Map, String)"() {
+        when:
+        def ok = session.GET(CBS, PATH)
+
+        then:
+        ok == SUCCESS
+        session.cbs.is(CBS)
+        session.method == 'GET'
+        session.path.is(PATH)
+        session.resource == null
+        session.cb == null
+    }
+
+    def "GET(String, Map, Closure)"() {
+        when:
+        def ok = session.GET(PATH, CBS, CB)
+
+        then:
+        ok == SUCCESS
+        session.cbs.is(CBS)
+        session.method == 'GET'
+        session.path.is(PATH)
+        session.resource == null
+        session.cb.is(CB)
+    }
+
+    def "GET(String, Map)"() {
+        when:
+        def ok = session.GET(PATH, CBS)
+
+        then:
+        ok == SUCCESS
+        session.cbs.is(CBS)
+        session.method == 'GET'
+        session.path.is(PATH)
+        session.resource == null
+        session.cb == null
+    }
+
+    def "GET(String, Closure)"() {
+        when:
+        def ok = session.GET(PATH, CB)
+
+        then:
+        ok == SUCCESS
+        session.cbs.isEmpty()
+        session.method == 'GET'
+        session.path.is(PATH)
+        session.resource == null
+        session.cb.is(CB)
+    }
+
+    def "GET(String)"() {
+        when:
+        def ok = session.GET(PATH)
+
+        then:
+        ok == SUCCESS
+        session.cbs.isEmpty()
+        session.method == 'GET'
+        session.path.is(PATH)
+        session.resource == null
+        session.cb == null
+    }
+
+    def "DELETE(Map, String, Closure)"() {
+        when:
+        def ok = session.DELETE(CBS, PATH, CB)
+
+        then:
+        ok == SUCCESS
+        session.cbs.is(CBS)
+        session.method == 'DELETE'
+        session.path.is(PATH)
+        session.resource == null
+        session.cb.is(CB)
+    }
+
+    def "DELETE(Map, String)"() {
+        when:
+        def ok = session.DELETE(CBS, PATH)
+
+        then:
+        ok == SUCCESS
+        session.cbs.is(CBS)
+        session.method == 'DELETE'
+        session.path.is(PATH)
+        session.resource == null
+        session.cb == null
+    }
+
+    def "DELETE(String, Map, Closure)"() {
+        when:
+        def ok = session.DELETE(PATH, CBS, CB)
+
+        then:
+        ok == SUCCESS
+        session.cbs.is(CBS)
+        session.method == 'DELETE'
+        session.path.is(PATH)
+        session.resource == null
+        session.cb.is(CB)
+    }
+
+    def "DELETE(String, Map)"() {
+        when:
+        def ok = session.DELETE(PATH, CBS)
+
+        then:
+        ok == SUCCESS
+        session.cbs.is(CBS)
+        session.method == 'DELETE'
+        session.path.is(PATH)
+        session.resource == null
+        session.cb == null
+    }
+
+    def "DELETE(String, Closure)"() {
+        when:
+        def ok = session.DELETE(PATH, CB)
+
+        then:
+        ok == SUCCESS
+        session.cbs.isEmpty()
+        session.method == 'DELETE'
+        session.path.is(PATH)
+        session.resource == null
+        session.cb.is(CB)
+    }
+
+    def "DELETE(String)"() {
+        when:
+        def ok = session.DELETE(PATH)
+
+        then:
+        ok == SUCCESS
+        session.cbs.isEmpty()
+        session.method == 'DELETE'
+        session.path.is(PATH)
+        session.resource == null
+        session.cb == null
+    }
+
+    def "PUT(Map, String, Resource, Closure)"() {
+        when:
+        def ok = session.PUT(CBS, PATH, RESOURCE, CB)
+
+        then:
+        ok == SUCCESS
+        session.cbs.is(CBS)
+        session.method == 'PUT'
+        session.path.is(PATH)
+        session.resource.is(RESOURCE)
+        session.cb.is(CB)
+    }
+
+    def "PUT(Map, String, Resource)"() {
+        when:
+        def ok = session.PUT(CBS, PATH, RESOURCE)
+
+        then:
+        ok == SUCCESS
+        session.cbs.is(CBS)
+        session.method == 'PUT'
+        session.path.is(PATH)
+        session.resource.is(RESOURCE)
+        session.cb == null
+    }
+
+    def "PUT(String, Resource, Map, Closure)"() {
+        when:
+        def ok = session.PUT(PATH, RESOURCE, CBS, CB)
+
+        then:
+        ok == SUCCESS
+        session.cbs.is(CBS)
+        session.method == 'PUT'
+        session.path.is(PATH)
+        session.resource.is(RESOURCE)
+        session.cb.is(CB)
+    }
+
+    def "PUT(String, Resource, Map)"() {
+        when:
+        def ok = session.PUT(PATH, RESOURCE, CBS)
+
+        then:
+        ok == SUCCESS
+        session.cbs.is(CBS)
+        session.method == 'PUT'
+        session.path.is(PATH)
+        session.resource.is(RESOURCE)
+        session.cb == null
+    }
+
+    def "PUT(String, Resource, Closure)"() {
+        when:
+        def ok = session.PUT(PATH, RESOURCE, CB)
+
+        then:
+        ok == SUCCESS
+        session.cbs.isEmpty()
+        session.method == 'PUT'
+        session.path.is(PATH)
+        session.resource.is(RESOURCE)
+        session.cb.is(CB)
+    }
+
+    def "PUT(String, Resource)"() {
+        when:
+        def ok = session.PUT(PATH, RESOURCE)
+
+        then:
+        ok == SUCCESS
+        session.cbs.isEmpty()
+        session.method == 'PUT'
+        session.path.is(PATH)
+        session.resource.is(RESOURCE)
+        session.cb == null
+    }
+
+    def "POST(Map, String, Resource, Closure)"() {
+        when:
+        def ok = session.POST(CBS, PATH, RESOURCE, CB)
+
+        then:
+        ok == SUCCESS
+        session.cbs.is(CBS)
+        session.method == 'POST'
+        session.path.is(PATH)
+        session.resource.is(RESOURCE)
+        session.cb.is(CB)
+    }
+
+    def "POST(Map, String, Resource)"() {
+        when:
+        def ok = session.POST(CBS, PATH, RESOURCE)
+
+        then:
+        ok == SUCCESS
+        session.cbs.is(CBS)
+        session.method == 'POST'
+        session.path.is(PATH)
+        session.resource.is(RESOURCE)
+        session.cb == null
+    }
+
+    def "POST(String, Resource, Map, Closure)"() {
+        when:
+        def ok = session.POST(PATH, RESOURCE, CBS, CB)
+
+        then:
+        ok == SUCCESS
+        session.cbs.is(CBS)
+        session.method == 'POST'
+        session.path.is(PATH)
+        session.resource.is(RESOURCE)
+        session.cb.is(CB)
+    }
+
+    def "POST(String, Resource, Map)"() {
+        when:
+        def ok = session.POST(PATH, RESOURCE, CBS)
+
+        then:
+        ok == SUCCESS
+        session.cbs.is(CBS)
+        session.method == 'POST'
+        session.path.is(PATH)
+        session.resource.is(RESOURCE)
+        session.cb == null
+    }
+
+    def "POST(String, Resource, Closure)"() {
+        when:
+        def ok = session.POST(PATH, RESOURCE, CB)
+
+        then:
+        ok == SUCCESS
+        session.cbs.isEmpty()
+        session.method == 'POST'
+        session.path.is(PATH)
+        session.resource.is(RESOURCE)
+        session.cb.is(CB)
+    }
+
+    def "POST(String, Resource)"() {
+        when:
+        def ok = session.POST(PATH, RESOURCE)
+
+        then:
+        ok == SUCCESS
+        session.cbs.isEmpty()
+        session.method == 'POST'
+        session.path.is(PATH)
+        session.resource.is(RESOURCE)
+        session.cb == null
+    }
+}
