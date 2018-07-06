a8fd508adac08def4176db41a08eaa6d770882aa
Mikko Värri <vmj@linuxbox.fi>
2018-07-07 01:03:48 +0300

    Version 0.0.7

--- groovy-upcloud-core/src/test/groovy/fi/linuxbox/upcloud/core/SessionSpec.groovy
+++ groovy-upcloud-core/src/test/groovy/fi/linuxbox/upcloud/core/SessionSpec.groovy
@@ -83,7 +83,7 @@ class SessionSpec extends Specification {
                         case 'Host':
                             return it.value == 'api.upcloud.com'
                         case 'User-Agent':
-                            return it.value.startsWith('Groovy UpCloud/0.0.7-SNAPSHOT ')
+                            return it.value.startsWith('Groovy UpCloud/0.0.7 ')
                         default:
                             return false
                     }
