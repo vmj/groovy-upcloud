1393b4f5768737507d712adc937651c34b34c618
Mikko Värri <vmj@linuxbox.fi>
2018-07-06 15:46:07 +0300

    Add ErrorSpec

---
+++ groovy-upcloud-resource/src/test/groovy/fi/linuxbox/upcloud/resource/ErrorSpec.groovy
@@ -0,0 +1,47 @@
+/*
+ * Groovy UpCloud library - Resource Module
+ * Copyright (C) 2018  <mikko@varri.fi>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package fi.linuxbox.upcloud.resource
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class ErrorSpec extends Specification {
+
+    @Unroll
+    def "ToString: #expected"() {
+        given:
+        def error = new Error(repr: [errorCode: code, errorMessage: message])
+
+        expect:
+        "$error" == expected
+
+        where:
+        code | message | expected
+        "EC" | "EM"    | "Error(errorCode: EC, errorMessage: EM)"
+        ''   | ''      | 'Error(errorCode: , errorMessage: )'
+        null | null    | 'Error()'
+    }
+
+    def "ToString: null"() {
+        given:
+        Error error = null
+
+        expect:
+        "$error" == 'null'
+    }
+}
